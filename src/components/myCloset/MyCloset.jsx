d5faf43300b83813a33fb3d1cbb892fcbe76bbbe
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-05-14 15:25:27 -0500

    upload addition and final touches

--- src/components/myCloset/MyCloset.jsx
+++ src/components/myCloset/MyCloset.jsx
@@ -12,43 +12,43 @@ export const MyCloset = ({ currentUser }) => {
     let navigate = useNavigate()
 
     useEffect(() => {
-        if ("id" in currentUser){ // this is checking to see if the object current user has the id key in it.
+        if ("id" in currentUser) { // this is checking to see if the object current user has the id key in it.
             getPostByUserId({ currentUser }).then((data) => { setUserPosts(data) })
         }
 
     }, [currentUser]);
 
-  
-    const handleClick =(item) =>{
+
+    const handleClick = (item) => {
         navigate("/my-outfit", { state: { item } })
-        
+
     }
 
     return (
         <>
-        <section className="header-of-page">
-            <section className="header-of-page">
-                <h1>My Closet</h1>
+            <section>
+                <section className="header-of-page">
+                    <h1>My Closet</h1>
                 </section>
-{userPosts.length ? 
-                <div className="post-photo">
-                    {userPosts.map((item) => {
-                        let picture = item.photo;
-                        return (
-                            <img key={item.id} data={item.seasonsId} src={picture} alt={picture}
-                            onClick={() => handleClick(item)}/>
-                        );
-                    })}
-                </div>
-:
-<section className="Upload-photo">
-    
-<div>Oops looks like your closet is empty.  Let's have you upload a photo and get started!</div>
-<button onClick={()=>navigate("/upload")}> Upload </button>
+                {userPosts.length ?
+                    <div className="post-photo">
+                        {userPosts.map((item) => {
+                            let picture = item.photo;
+                            return (
+                                <img key={item.id} data={item.seasonsId} src={picture} alt={picture}
+                                    onClick={() => handleClick(item)} />
+                            );
+                        })}
+                    </div>
+                    :
+                    <section className="Upload-photo">
 
-</section>
-}
-            
+                        <div>Oops looks like your closet is empty.  Let's have you upload a photo and get started!</div>
+                        <button onClick={() => navigate("/upload")}> Upload </button>
+
+                    </section>
+                }
+
             </section>
         </>
     )
--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -3,11 +3,25 @@ import { getAllSeasons } from "../../services/seasonsServices.jsx"
 import { useNavigate } from "react-router-dom"
 import { savePostToDatabase } from "../../services/PostService.jsx"
 import "./upload.css"
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage, responsive, placeholder } from "@cloudinary/react";
+import { UploadWidget } from "./UploadWidget.jsx"
 
 export const Upload = ({ currentUser }) => {
     const [photoURL, setPhotoURL] = useState("")
     const [selectedSeason, setSelectedSeason] = useState({})
     const [allSeasons, setAllSeasons] = useState([])
+
+    const [publicId, setPublicId] = useState("");
+    // Replace with your own cloud name
+    const [cloudName] = useState(import.meta.env.VITE_CLOUD_NAME);
+    // Replace with your own upload preset
+    const [uploadPreset] = useState(import.meta.env.VITE_PRESET);
+
+    const [uwConfig] = useState({
+        cloudName,
+        uploadPreset,
+    });
 
     const navigate = useNavigate()
 
@@ -24,45 +38,106 @@ export const Upload = ({ currentUser }) => {
         }
         savePostToDatabase(newPost)
         // .then({
-            navigate("/my-closet")
+        navigate("/my-closet")
         // })
     }
 
+    // Create a Cloudinary instance and set your cloud name.
+    const cld = new Cloudinary({
+        cloud: {
+            cloudName,
+        },
+    });
 
+    const myImage = cld.image(publicId);
 
-
-        return (<>
+    return (
+        <div>
             <section className="header-of-page">
                 <h1>Upload Photo</h1>
-                </section>
-                <div className="middle">
-                    {/* <form onSubmit={savePhoto}> */}
-                        <input
-                            type="url"
-                            value={photoURL}
-                            onChange={(evt) => setPhotoURL(evt.target.value)}
-                            className="form-control"
-                            placeholder="Add Photo Link"
-                            required
-                            autoFocus
-                        />
-                        <label htmlFor="seasons"> Choose a Season for this fit: </label>
-                        <select name="seasons" id="seasons" onChange={(evt) => setSelectedSeason(evt.target.value)}>
-                            <option defaultValue>Choose a Season</option>
+            </section>
+            <div className="middle">
+                <label htmlFor="seasons"> Choose a Season for this fit: </label>
+                <select className="dropDown" name="seasons" id="seasons" onChange={(evt) => setSelectedSeason(evt.target.value)}>
+                    <option defaultValue>Choose a Season</option>
 
-                            {allSeasons.map((seasonItem) => {
-                                return (
-                                    <option key={seasonItem.id} value={seasonItem.id}>{seasonItem.name}</option>
+                    {allSeasons.map((seasonItem) => {
+                        return (
+                            <option key={seasonItem.id} value={seasonItem.id}>{seasonItem.name}</option>
 
-                                )
-                            })}
-                        </select>
+                        )
+                    })}
+                </select>
+            </div>
+            <UploadWidget
+                uwConfig={uwConfig}
+                setPublicId={setPublicId}
+                // photoURL={photoURL}
+                setPhotoURL={setPhotoURL}
+            />
 
-                       
+            <div style={{ width: "800px" }}>
+                <AdvancedImage
+                    style={{ maxWidth: "100%" }}
+                    cldImg={myImage}
+                    plugins={[responsive(), placeholder()]}
+                />
+            </div>
+            <button className="button-Submit" type="submit" onClick={savePost} >Submit</button>
+        </div>
+    );
 
-                    {/* </form> */}
-                </div>
-                <button className="button-photo" type="submit" onClick={savePost} >Submit Photo</button>
-        </>
-        )
-    }
+    // const navigate = useNavigate()
+
+    // useEffect(() => {
+    //     getAllSeasons().then((data) => { setAllSeasons(data) })
+
+    // }, [currentUser])
+
+    // const savePost = () => {
+    //     const newPost = {
+    //         "userId": currentUser.id,
+    //         "photo": photoURL,
+    //         "seasonsId": selectedSeason
+    //     }
+    //     savePostToDatabase(newPost)
+    //     // .then({
+    //         navigate("/my-closet")
+    //     // })
+    // }
+
+    // return (<>
+    //     <section className="header-of-page">
+    //         <h1>Upload Photo</h1>
+    //         </section>
+    //         <div className="middle">
+    //             {/* <form onSubmit={savePhoto}> */}
+    //                 <input
+    //                     type="url"
+    //                     value={photoURL}
+    //                     onChange={(evt) => setPhotoURL(evt.target.value)}
+    //                     className="form-control"
+    //                     placeholder="Add Photo Link"
+    //                     required
+    //                     autoFocus
+    //                 />
+    // <label htmlFor="seasons"> Choose a Season for this fit: </label>
+    // <select name="seasons" id="seasons" onChange={(evt) => setSelectedSeason(evt.target.value)}>
+    //     <option defaultValue>Choose a Season</option>
+
+    //     {allSeasons.map((seasonItem) => {
+    //         return (
+    //             <option key={seasonItem.id} value={seasonItem.id}>{seasonItem.name}</option>
+
+    //         )
+    //     })}
+    // </select>
+
+
+
+    //             {/* </form> */}
+    //         </div>
+    //         <button className="button-photo" type="submit" onClick={savePost} >Submit Photo</button>
+    // </>
+    // )
+}
